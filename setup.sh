d4aec4d1e44ac7cd11fa8de9c785e9e73aace232
Facundo Santiago <santiagof@outlook.com>
2022-05-13 12:46:23 -0400

    fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 PROJECT_NAME="carpricer"
-CONDA_ENV_NAME="transformers-torch-19-dev"
+CONDA_ENV_NAME="carpricer"
 CONDA_ENV_PYTHON="3.8.5"
 CONDA_FILE=""
 PIP_REQUIREMENTS=".aml/jobs/docker-context/requirements.txt"
-
-echo "Configuring PYTHONPATH for the project"
-cat >> $(python -m site --user-site)/$PROJECT_NAME.pth <<EOF
-$PWD/src
-EOF
 
 echo "Creating a conda environment"
 if [ -z "$CONDA_FILE" ]; then
@@ -15,9 +10,15 @@ if [ -z "$CONDA_FILE" ]; then
 else
     conda env create --name $CONDA_ENV_NAME -f $CONDA_FILE
 fi
+. $(conda info --json | jq -r '.root_prefix')/etc/profile.d/conda.sh
 conda activate $CONDA_ENV_NAME
 conda install jupyter ipykernel
 if [ -n "$PIP_REQUIREMENTS" ]; then
-    pip install -r $PIP_REQUIREMENTS
+    pip install -y -r $PIP_REQUIREMENTS
 fi
 python -m ipykernel install --user --name $CONDA_ENV_NAME --display-name "Python ($CONDA_ENV_NAME)"
+
+echo "Configuring PYTHONPATH for the project"
+cat >> $(python -m site --user-site)/$PROJECT_NAME.pth <<EOF
+$PWD/src
+EOF
